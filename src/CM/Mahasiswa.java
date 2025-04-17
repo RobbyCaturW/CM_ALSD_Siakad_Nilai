21efce4665dd6eebc0fd2069a4c029a7c7cb5010
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2025-04-17 08:47:43 +0700

    Mahasiswa

---
+++ src/CM/Mahasiswa.java
@@ -0,0 +1,18 @@
+package CM_ALSD_Siakad_Nilai.CM;
+
+public class Mahasiswa {
+    
+    String NIM;
+    String nama;
+    String prodi;
+
+    public Mahasiswa(String NIM, String nama, String prodi){
+        this.NIM = NIM;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
+    public void tampilMahasiswa(){
+        System.out.println("NIM: " + NIM + " | Nama " + nama + " | Prodi " + prodi);
+    }
+}
---
+++ src/CM/Main.java
@@ -0,0 +1,66 @@
+package semester2.CM_ALSD_Siakad_Nilai.src;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pilihan;
+
+        Mahasiswa[] mhs = {
+            new Mahasiswa("22001", "Ali Rahman", "Informatika"),
+            new Mahasiswa("22002", "Budi Santoso", "Informatika"),
+            new Mahasiswa("22003", "Citra Dewi", "Sistem Informasi Bisnis")
+
+        };
+
+
+
+
+
+
+
+
+        do {
+            System.out.println("=== MENU SISTEM AKADEMIK ===");
+            System.out.println("1. Tampilkan Daftar Mahasiswa");
+            System.out.println("2. Tampilkan Daftar Mata Kuliah");
+            System.out.println("3. Tampilkan Data Penilaian");
+            System.out.println("4. Ururtkan Mahasiswa Berdasarkan Nilai Akhir");
+            System.out.println("5. Cari Mahasiswa Berdasarkan NIM");
+            System.out.println("0. keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = sc.nextInt();
+            sc.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    
+
+                    break;
+            
+                case 2:
+
+
+                    break;
+
+                case 3:
+
+
+                    break;
+                
+                case 4:
+
+
+                    break;
+
+                case 5:
+
+
+                    break;
+            }
+
+
+        }while (pilihan != 0);
+    }
+}
